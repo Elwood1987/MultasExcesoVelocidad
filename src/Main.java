125220b4ff5f925aa3804e498323a278931c7123
Elwood1987 <cristian.castro.lara@gmail.com>
2021-10-20 21:49:30 -0500

    Initial commit from MultasExcesoVelocidad project

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        SpeedingTicket s = new SpeedingTicket();
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true) {
+            System.out.println("Ingrese los valores a validar");
+            String data = input.readLine();
+            System.out.println(s.init(data));
+        }
+    }
+}
---
+++ src/SpeedingTicket.java
@@ -0,0 +1,50 @@
+public class SpeedingTicket {
+
+    public String init(String data) {
+        String[] splitData = data.split(" ");
+        int distance = Integer.parseInt(splitData[0]);
+        int maximumSpeed =Integer.parseInt(splitData[1]);
+        int time = Integer.parseInt(splitData[2]);
+        String validate = validate(distance, maximumSpeed, time);
+        if (validate.equals("")) {
+            return calculate(distance, maximumSpeed, time);
+        } else {
+            return validate;
+        }
+    }
+
+    public String calculate(int distance, int maximumSpeed, int time) {
+        double calculateSpeed = getSpeed(distance, time);
+        if(calculateSpeed <= maximumSpeed){
+            return Result.OK.name();
+            } else if (calculateSpeed >= maximumSpeed * 1.20) {
+            return Result.CURSO_SENSIBILIZACION.name();
+        } else {
+            return Result.MULTA.name();
+        }
+    }
+
+    public double getSpeed(int distance, int time) {
+        float speed =  ((float) distance / (float) time) * 3.6f;
+        return speed;
+    }
+
+    public String validate(int distance, int maximumSpeed, int time) {
+        if (distance == 0 && maximumSpeed == 0 && time == 0) {
+            return Result.ERROR.name();
+        }
+
+        if (distance <= 0 || maximumSpeed <= 0 || maximumSpeed <= 0) {
+            return Result.ERROR.name();
+        }
+        return "";
+    }
+
+    enum Result {
+        OK,
+        MULTA,
+        CURSO_SENSIBILIZACION,
+        ERROR;
+    }
+
+}
